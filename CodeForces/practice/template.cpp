cc9b7c9c9852cc852f70617ff93c3659a2afe7cd
Sanath Kumar <sanatku@amazon.com>
2022-08-15 17:23:51 +0530

    Added template file

---
+++ CodeForces/practice/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "algo/debug.h"
+
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define debug(...) 42
+#else
+#include "algo/debug.h"
+#endif
+
+int main() {
+
+    return 0;
+}
